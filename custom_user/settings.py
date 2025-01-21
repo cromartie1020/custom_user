d480088896e39dfb75795c271291542caef533a4
cromartie1020 <hrc245@mail.com>
2025-01-21 16:52:05 -0500

    Email setup in settings.py

--- custom_user/settings.py
+++ custom_user/settings.py
@@ -131,4 +131,10 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER=config['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD=config['EMAIL_HOST_PASSWORD']
