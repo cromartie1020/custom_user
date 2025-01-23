0c6f527f1fa51886a135e0b9d4e15a65a398e285
cromartie1020 <hrc245@mail.com>
2025-01-23 16:24:51 -0500

    Add login_required

--- articles/views.py
+++ articles/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render,redirect
 from . models import Article
 from .forms import ArticleForm 
+from django.contrib.auth.decorators import login_required
+
 def article_list(request):
     articles=Article.objects.all()
     context = {
         'articles':articles
     }
     return render(request, 'articles/article_list.html', context)
+@login_required
 def article_detail(request,id):
     article=Article.objects.get(id=id)
     context ={
         'article':article
     }
     return render(request, 'articles/article_detail.html',context)
+@login_required
 def article_new(request):
     if request.method == ('POST' or  None):
         form = ArticleForm(request.POST)
@@ -29,7 +33,7 @@ def article_new(request):
     }
 
     return render(request,'articles/article_new.html',context)    
-
+@login_required
 def article_update(request,id):
     article= Article.objects.get(id = id)
     form = ArticleForm(request.POST or None, instance= article)
@@ -42,12 +46,12 @@ def article_update(request,id):
     }
     return render(request, 'articles/article_update.html', context)
 
-
+@login_required
 def article_delete(request,id):
     article=Article.objects.get(id =id )
     article.delete()
     return render(request, 'articles/article_delete.html')
-
+@login_required
 def article_confirm_delete(request,id):
     article=Article.objects.get(id =id )
     
