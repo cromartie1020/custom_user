d3a63d31b7f0c5d02ad43db29405e2bd8ba94851
cromartie1020 <hrc245@mail.com>
2025-01-22 19:51:21 -0500

    Crud operations

--- articles/urls.py
+++ articles/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('',views.article_list, name='article_list'),
 
     path('<int:id>/', views.article_detail, name='article_detail'),
+    path('detail/<int:id>/', views.article_update, name='article_update'),
+    path('new/', views.article_new, name='article_new'),
+    path('delete/<int:id>/', views.article_delete, name='article_delete'),
+    
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . models import Article
+from .forms import ArticleForm 
 def article_list(request):
     articles=Article.objects.all()
     context = {
@@ -7,10 +8,42 @@ def article_list(request):
     }
     return render(request, 'articles/article_list.html', context)
 def article_detail(request,id):
-    pass
+    article=Article.objects.get(id=id)
+    context ={
+        'article':article
+    }
+    return render(request, 'articles/article_detail.html',context)
+def article_new(request):
+    if request.method == ('POST' or  None):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('article_list')
+    else:
+
+
+        form=ArticleForm()
+
+    context= {
+        'form':form
+    }
+
+    return render(request,'articles/article_new.html',context)    
 
 def article_update(request,id):
-    pass
+    article= Article.objects.get(id = id)
+    form = ArticleForm(request.POST or None, instance= article)
+    if form.is_valid():
+        article=form.save(commit=False)
+        article.save()
+        return redirect('article_list') 
+    context = {
+        'form':form
+    }
+    return render(request, 'articles/article_update.html', context)
+
 
 def article_delete(request,id):
-    pass
+    article=Article.objects.get(id =id )
+    article.delete()
+    return render(request, 'articles/article_delete.html')
